84eb178be22779c6a7d32343c110ca17a7bf37f8
Cristina Iannone <iannonecristina@gmail.com>
2017-03-28 09:57:43 +0100

    Add exercise 2

--- directory.rb
+++ directory.rb
@@ -40,7 +40,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].start_with? "D"
+     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 def print_footer(names)
