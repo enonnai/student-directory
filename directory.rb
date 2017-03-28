cd22cf842d9c3f50e6d91969785e0416ebd78618
Cristina Iannone <iannonecristina@gmail.com>
2017-03-28 18:08:17 +0100

    Add Ex. 7

--- directory.rb
+++ directory.rb
@@ -1,53 +1,45 @@
 def input_students
-  puts "Please enter the required information for each student"
-  puts "To finish, please type in 'done' when asked for the NAME of the next sudent."
-  #create an empty array
+  puts "Please fill in the form for each student."
+  puts "To finish, please type in 'done' when asked for NAME & COHORT."
+  puts
   students = []
-  #get the first name
-  puts "NAME:"
-  name = gets.chomp
-  puts "COUNTRY OF BIRTH:"
-  country = gets.chomp
-  puts "HOBBIES:"
-  hobbies = gets.chomp
-  puts "HEIGHT:"
-  height = gets.chomp
-  #while the name is not empty, repeat this code
-    while !name.empty? do
-      #add the student hash to the array
-      students << {name: name, cohort: :november, hobbies: hobbies, country: country, height: height}
-      puts "Now we have #{students.count} students"
-      #get another username from the user
-      puts "NAME:"
-      name = gets.chomp
+  default = "TBD"
+  answer = ""
+
+name = ""
+    while name != "done" do
+      puts "NAME & COHORT - please insert a dot between the two, (e.g. John Doe. November) or type in 'done.' to finish."
+      name, cohort = gets.split('.')
       break if name == "done"
-      puts "COUNTRY OF BIRTH:"
+      name.to_s
+        if cohort == nil
+          cohort = default
+          cohort_sym = cohort.to_sym
+        end
+      cohort_sym = cohort.downcase.to_sym
+      puts "COUNTRY OF BIRTH"
       country = gets.chomp
-      puts "HOBBIES:"
+      puts "HOBBIES"
       hobbies = gets.chomp
-      puts "HEIGHT:"
+      puts "HEIGHT"
       height = gets.chomp
+      puts "Are you happy to submit? Type in 'yes' or 'no' to fill in the info again."
+      answer = gets.chomp
+        if answer == "no"
+          next
+        else
+      students << {name: name, cohort: cohort_sym, hobbies: hobbies, country: country, height: height}
+      puts "Now we have #{students.count} students"
+      puts
+      #puts cohort_sym.is_a? Symbol
+        next
+    end
+      #get another username from the user
     end
 #return the array of students
 students
 end
 
-# let's put all students into an array
-=begin
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
-=end
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -65,13 +57,14 @@ end
 def print(students)
   i = 0
   while i <= students.length-1
-    puts " #{students[i][:name]}, from #{students[i][:country]}. Hobbies: #{students[i][:hobbies]}. Height: #{students[i][:height]}. (#{students[i][:cohort].capitalize} cohort) ".center(120, '*')
+    puts " #{students[i][:name].capitalize}, from #{students[i][:country].capitalize}. Hobbies: #{students[i][:hobbies]}. Height: #{students[i][:height]}. (Cohort:#{students[i][:cohort].upcase}) ".center(120, '*')
     i += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "-------------"
+  puts "Overall, we have #{names.count} great students.\n\nNB: 'TBD' stands for 'To Be Determined'"
 end
 #nothing happens until we call the methods
 students = input_students
